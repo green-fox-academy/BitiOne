21ed5ff04e41e2ce8b8ae586628eb79410b98171
BitiOne <bittner.school@gmail.com>
2020-11-23 14:31:48 +0100

    Started the day with three array tasks

---
+++ week02/day06/compare-lenght.ts
@@ -0,0 +1,19 @@
+'use strict';
+  
+// - Create a variable named `firstList`
+//   with the following content: `[1, 2, 3]`
+// - Create a variable named `secondList`
+//   with the following content: `[4, 5]`
+// - Log "p2 is longer" to the console if `secondList` has more elements than
+//   `firstList`
+
+let firstList = [1, 2, 3];
+let secondList = [4, 5];
+
+if (firstList.length > secondList.length) {
+    console.log(firstList.length);
+    console.log('firstList longer than secondList');
+} if (firstList.length < secondList.length) {
+    console.log(secondList.length);
+    console.log('secondList longer than firstList');
+}
