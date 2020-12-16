5a1fefc35fe8416cf066f69b89b7315e179f6051
BitiOne <bittner.school@gmail.com>
2020-12-16 13:56:08 +0100

    Mentor file finished

---
+++ week04/day16/01-mentor.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+import { Person } from "./01-person";
+
+export class Mentor extends Person {
+  protected _level: string;
+
+  constructor(name?: string, age?: number, gender?: string, level?: string) {
+    super(name, age, gender);
+    this._level = level ?? 'Intermidiate';
+  }
+
+  public getGoal(): string {
+    return `My goal is: Educate brilliant junior software developers.`;
+  }
+
+  public introduce(): string {
+    return `Hi, I'm ${super._name}, a ${super._age} year old ${super._gender} ${this._level} mentor.`;
+  }
+}
