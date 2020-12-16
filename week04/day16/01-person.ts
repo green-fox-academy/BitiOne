cc19fa037eeccfd2a7d26307dbeaf5ae9d26941d
BitiOne <bittner.school@gmail.com>
2020-12-16 11:40:04 +0100

    Person class done

--- week04/day16/01-person.ts
+++ week04/day16/01-person.ts
@@ -1,2 +1,21 @@
 'use strict';
 
+export class Person {
+  protected _name: string;
+  protected _age: number;
+  protected _gender: string;
+
+  constructor(name?: string, age?: number, gender?: string) {
+    this._name = name ?? 'Jane Doe';
+    this._age = age ?? 30;
+    this._gender = gender ?? 'female';
+  }
+
+  public introduce(): string {
+    return `Hi, I'm ${this._name}, a ${this._age} year old ${this._gender}`;
+  }
+
+  public getGoal(): string {
+    return `My goal is: Live for the moment!`;
+  }
+}
