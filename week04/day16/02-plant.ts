d750bd11dd23927870dadcc46baea2856296980f
BitiOne <bittner.school@gmail.com>
2020-12-16 15:37:45 +0100

    02-plant.ts basic parameters and constuctor set.

--- week04/day16/02-plant.ts
+++ week04/day16/02-plant.ts
@@ -1,2 +1,15 @@
 'use strict';
 
+export class Plant {
+  protected _color: string;
+  protected _plantType: string;
+  protected _amountsOfWater: number;
+  protected _needWater: boolean;
+
+  constructor(color: string, plantType: string) {
+    this._color = color;
+    this._plantType = plantType;
+    this._amountsOfWater = 0;
+    this._needWater = true;
+  }
+}
