f2e1346d5285bcf99a5d8914214e081e5dd22bff
BitiOne <bittner.school@gmail.com>
2020-12-16 14:38:57 +0100

    01-cohort.ts finished, add and finished test.ts file.

--- week04/day16/01-cohort.ts
+++ week04/day16/01-cohort.ts
@@ -1,17 +1,28 @@
 'use strict';
 
 import { Student } from "./01-student";
+import { Mentor } from "./01-mentor";
 
 export class Cohort {
   protected _name: string;
-  protected _students: string[] = [];
-  protected _mentors: string[] = [];
+  protected _students: Student[];
+  protected _mentors: Mentor[];
 
   constructor(name: string) {
     this._name = name;
+    this._students = [];
+    this._mentors = [];
   }
 
-  public addStudent(Student: Student[]): void {
-    this._students.push(Student);
+  public addStudent(student: Student): void {
+    this._students.push(student);
+  }
+
+  public addMentor(mentor: Mentor): void {
+    this._mentors.push(mentor);
+  }
+
+  public info(): string {
+    return `The ${this._name} cohort has ${this._students.length} students and ${this._mentors.length} mentors.`;
   }
 }
