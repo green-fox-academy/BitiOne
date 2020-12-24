a147463d1602ef574ab7415a18823f930562efc0
BitiOne <bittner.school@gmail.com>
2020-12-24 09:55:01 +0100

    Day 07 task 08 done.

--- week02/day07/08-map-introduction-1.ts
+++ week02/day07/08-map-introduction-1.ts
@@ -1,7 +1,29 @@
 'use strict';
 
-let map = {
-  1: 'caracters'
-}
+let map = {};
 
+map['97'] = 'a';
+map['98'] = 'b';
+map['99'] = 'c';
+map['65'] = 'A';
+map['66'] = 'B';
+map['67'] = 'C';
+console.log(map);
+
+console.log(Object.keys(map));
+
+console.log(Object.values(map));
+
+map['68'] = 'D';
+console.log(map);
+
+console.log(Object.keys(map).length);
+
+console.log(map[99]);
+
+delete map[97];
+
+console.log(map.hasOwnProperty('100'));
+
+map = {};
 console.log(map);
