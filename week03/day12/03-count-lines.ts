864dd007291e73e3b2400f269f32d8402d55b41b
BitiOne <bittner.school@gmail.com>
2020-12-25 17:04:27 +0100

    Day 12 task 03 done.

--- week03/day12/03-count-lines.ts
+++ week03/day12/03-count-lines.ts
@@ -1,7 +1,29 @@
 'use strict';
 
+const fs = require('fs');
+
 // Write a function that takes a filename as string,
 // then returns the number of lines the file contains.
 // It should return zero if it can't open the file, and
 // should not raise any error.
 
+let fileContent: string;
+
+function lineCounter(givenFileName: string) {
+
+  let splittedData: string[] = [];
+
+  try {
+    fileContent = fs.readFileSync(givenFileName, 'utf-8');
+  }
+
+  catch(e) {
+    console.log(`File does not exist.`);
+  }
+
+  splittedData = fileContent.split('\n');
+
+  console.log(`The ${givenFileName} file have ${splittedData.length} lines.`);
+}
+
+lineCounter('my-file.txt');
