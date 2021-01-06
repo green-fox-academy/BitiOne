42da9d318f813171470b313294c07676fe7c623b
BitiOne <bittner.school@gmail.com>
2021-01-06 14:24:25 +0100

    Day 27 task 01 done.

---
+++ week06/day27/01-queryselector-1.js
@@ -0,0 +1,20 @@
+const king = document.querySelector('.asteroid');
+console.log(king);
+
+const businessLamp = document.querySelectorAll('.big');
+
+businessLamp.forEach((elem) => {
+  console.log(elem);
+});
+
+const conceitedKing = document.querySelectorAll('#b325, .asteroid.b326');
+
+conceitedKing.forEach((elem) => {
+  alert(elem);
+});
+
+const noBusiness = document.querySelectorAll('#b325, .asteroid.b326, .asteroid.b329.big');
+
+noBusiness.forEach((elem) => {
+  console.log(elem);
+});
