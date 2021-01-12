5958dcf53f196293f4513e2ce53e2c13f0fe151b
BitiOne <bittner.school@gmail.com>
2021-01-12 14:18:05 +0100

    Task doubling done.

--- week07/day32/app.js
+++ week07/day32/app.js
@@ -11,6 +11,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+  const doubled = {};
+  let received = req.query.input;
+  let result = received * 2;
+  doubled.received = Number(received);
+  doubled.result = result;
+  if(received === undefined) {
+    res.json({'error': "Please provide an input!"});
+  } else {
+    res.json(doubled);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Listening port: ${PORT}.`);
 });
