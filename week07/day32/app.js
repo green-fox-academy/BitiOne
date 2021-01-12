a3861d34f84093fd4bdae11e1b6b643ed7da9def
BitiOne <bittner.school@gmail.com>
2021-01-12 15:06:51 +0100

    Task AppendA done (I think).

--- week07/day32/app.js
+++ week07/day32/app.js
@@ -13,10 +13,13 @@ app.get('/', (req, res) => {
 
 app.get('/doubling', (req, res) => {
   const doubled = {};
+
   let received = req.query.input;
   let result = received * 2;
+
   doubled.received = Number(received);
   doubled.result = result;
+
   if(received === undefined) {
     res.json({'error': "Please provide an input!"});
   } else {
@@ -27,9 +30,11 @@ app.get('/doubling', (req, res) => {
 app.get('/greeter', (req, res) => {
   let name = req.query.name;
   let title = req.query.title;
+
   const welcomeMessage = {
     "welcome_message": `Oh, hi there ${name}, my dear ${title}!`
   };
+
   if(name === undefined && title === undefined) {
     res.status(400).json({'error': "Please provide a name and a title!"});
   } else  if(name === undefined) {
@@ -40,6 +45,20 @@ app.get('/greeter', (req, res) => {
     res.json(welcomeMessage);
   }
 });
+
+app.get('/appenda/:appendable', (req, res) => {
+  let appendable = req.params.appendable;
+
+  const appenda = {
+    'appended': `${appendable}a`
+  };
+
+    res.json(appenda);
+});
+/*
+app.get('/appenda', (req, res) => {
+  res.status(404).send({'error': 'Please provide an appendable text!'});
+});*/
 
 app.listen(PORT, () => {
   console.log(`Listening port: ${PORT}.`);
