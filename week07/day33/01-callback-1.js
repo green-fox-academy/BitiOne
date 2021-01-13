f9fcc504dac081d8de3eea8a584b8c754d35116a
BitiOne <bittner.school@gmail.com>
2021-01-13 15:11:26 +0100

    Day 33 task 01 done.

---
+++ week07/day33/01-callback-1.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const mapWith = (array, callback) => {
+  let output = [];
+
+  for(let number of array) {
+    output.push(callback(number));
+  }
+  // The mapWith() function should iterate over the given array and call the callback function on every element.
+  // It stores the callback return values in the output.
+  // The mapWith() should return with the output array.
+
+  return output;
+}
+
+const addOne = (number) => {
+  return number + 1;
+}
+
+// Exercise 1:
+
+console.log(mapWith([1, 2, 3], addOne));
+//expected result: [2, 3, 4]
+
+// Exercise 2:
+
+// Create a callback function which removes every second character from a string
+
+const words = ['map', 'reduce', 'filter'];
+
+const removeSecondLetter = (string) => {
+  let word = string.split('');
+  let splittedText = [];
+
+  for(let i = 0; i < word.length; i++) {
+    if(i % 2 === 0) {
+      splittedText.push(word[i]);
+    }
+  }
+
+  return splittedText.join('');
+  
+}
+
+console.log(mapWith(words, removeSecondLetter));
+// expected result: ['mp','rdc', 'fle']
