c1a2ee8397be734e5f9808f24f33d007f81e17dd
BitiOne <bittner.school@gmail.com>
2021-01-16 12:22:24 +0100

    Day 35 reddit task - server started.

--- week07/day35/server.js
+++ week07/day35/server.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+const PORT = 3000;
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'hooah2020',
+  database: 'reddit',
+});
+
+conn.connect((err) =>{
+  if(err) {
+    console.log(err.toString());
+  }
+  console.log('Connected to MySQL.');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT);
