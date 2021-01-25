681ddfd622682d80b1dc83fe98cbb65391e74a7c
BitiOne <bittner.school@gmail.com>
2021-01-25 15:38:28 +0100

    Day 41 - task 02 - done.

--- week09/day41/promise/program.js
+++ week09/day41/promise/program.js
@@ -1,8 +1,20 @@
 'use strict';
 
-// task 01--------------------------------------------
+require('es6-promise');
+
+/* task 01--------------------------------------------
 setTimeout(function(){
   console.log('TIMED OUT!');
 }, 300);
+*/
 
-/* task 02--------------------------------------------
+// task 02--------------------------------------------
+var promise = new Promise(function (fulfill, reject) {
+  setTimeout(function() {
+    fulfill('FULFILLED!');
+  }, 300);
+});
+
+promise.then((result) => {
+  console.log(result);
+});
