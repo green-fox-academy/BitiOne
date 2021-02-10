e71c3c81765825c31d4bad35a609f5d12259209a
BitiOne <bittner.school@gmail.com>
2021-02-10 11:42:06 +0100

    Day 34 - Bookstore - 'All books with full data' task done.

--- week07/day34/public/index.js
+++ week07/day34/public/index.js
@@ -21,8 +21,30 @@ window.onload = () => {
   })
   .catch(err => {
     console.log('/book get error!');
+  });
+
+  fetch('/information', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
   })
-}
+  .then(response => {
+    if(response.status !== 200) {
+      throw new Error('Nincs válasz!');
+    } else {
+      return response;
+    }
+  })
+  .then(responseObject => responseObject.json())
+  .then(responseObjecttoJson => {
+    console.log(responseObjecttoJson);
+    createNewTableElements(responseObjecttoJson);
+  })
+  .catch(err => {
+    console.log('/book get error!');
+  });
+};
 
 function createNewListElements(listOfBooks) {
   const containerDiv = document.querySelector('#container');
@@ -31,5 +53,33 @@ function createNewListElements(listOfBooks) {
     const listElement = document.createElement('li');
     listElement.innerText = listOfBooks[i].book_name;
     containerDiv.appendChild(listElement);
+  }
+}
+
+function createNewTableElements(informationsOfBooks) {
+  const bodyOfTable = document.querySelector('#tableBody');
+
+  for(let i = 0; i < informationsOfBooks.length; i++) {
+    const newTableRow = document.createElement('tr');
+
+    const newTableCellTitle = document.createElement('td');
+    const newTableCellAuthor = document.createElement('td');
+    const newTableCellCategory = document.createElement('td');
+    const newTableCellPublisher = document.createElement('td');
+    const newTableCellPrice = document.createElement('td');
+
+    newTableCellTitle.innerText = informationsOfBooks[i].book_name;
+    newTableCellAuthor.innerText = informationsOfBooks[i].aut_name;
+    newTableCellCategory.innerText = informationsOfBooks[i].cate_descrip;
+    newTableCellPublisher.innerText = informationsOfBooks[i].pub_name;
+    newTableCellPrice.innerText = informationsOfBooks[i].book_price;
+
+    newTableRow.appendChild(newTableCellTitle);
+    newTableRow.appendChild(newTableCellAuthor);
+    newTableRow.appendChild(newTableCellCategory);
+    newTableRow.appendChild(newTableCellPublisher);
+    newTableRow.appendChild(newTableCellPrice);
+
+    bodyOfTable.appendChild(newTableRow);
   }
 }
--- week07/day34/server.js
+++ week07/day34/server.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const express = require('express');
-
 const app = express();
 const mysql = require('mysql');
 require('dotenv').config();
@@ -27,7 +26,7 @@ conn.connect((err) => {
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
-})
+});
 
 app.get('/book', (req, res) => {
   conn.query('SELECT book_name FROM book_mast;', (err, rows) => {
@@ -39,6 +38,18 @@ app.get('/book', (req, res) => {
       res.json(rows);
     }
   })
-})
+});
+
+app.get('/information', (req, res) => {
+  conn.query('SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast INNER JOIN author ON book_mast.aut_id = author.aut_id INNER JOIN category ON book_mast.cate_id = category.cate_id INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;', (err, rows) => {
+    if(err) {
+      console.log(err.toString());
+      res.status(500).json({ 'error': 'Can not read database!' });
+      return;
+    } else {
+      res.json(rows);
+    }
+  })
+});
 
 app.listen(PORT);
